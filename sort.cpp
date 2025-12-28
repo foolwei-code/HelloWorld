67d1e0e9d105a209aa0dbf5039733cc238d01b3d
ubuntu-wrg <123456@qq.com>
2025-12-28 22:01:12 +0800

    完成了sort.cpp中sort的功能

--- sort.cpp
+++ sort.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 void sort(int*arr,int size)
 {
+   for(int i=0;i<size;i++)
+   {
+      for(int j=i+1;j<size;j++)
+	      if(arr[i]>arr[j])
+		  swap(arr[i],arr[j]);
+   }
 }
 int main()
 {
